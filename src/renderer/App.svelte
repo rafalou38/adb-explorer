e4103c02a06adce07e78141bcc2c970015adff57
Rafael <rafael.martinezcalvo@orange.fr>
2021-03-27 18:24:57 +0100

    done navbar

--- src/renderer/App.svelte
+++ src/renderer/App.svelte
@@ -1,30 +1,19 @@
 <script lang="ts">
-	export let name: string;
+  import Navbar from "./components/explorer/navbar.svelte";
+  let path = "/sdcard/Download/Received/bluetooth/files";
 </script>
 
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <Navbar {path} />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
---
+++ src/renderer/components/explorer/navbar.svelte
@@ -0,0 +1,100 @@
+<!-- TODO show less items if not enough place -->
+<script lang="ts">
+  import Ripple from "@smui/ripple";
+
+  export let path: string;
+  let typing = false;
+
+  function gotoPath(index: number) {
+    let asaray = path.split("/").filter((e) => e);
+    asaray.splice(index + 1, asaray.length - index);
+    path = "/" + asaray.join("/");
+  }
+  function startTyping(e: MouseEvent) {
+    if (e.target.classList.contains("nav")) {
+      console.log("start typing");
+      typing = true;
+    }
+  }
+  function stopTyping(e: MouseEvent) {
+    if (!e.target.classList.contains("nav")) {
+      console.log("stopped typing");
+      typing = false;
+    }
+  }
+  function init(e: HTMLInputElement) {
+    e.focus();
+  }
+</script>
+
+<svelte:body on:click={stopTyping} />
+
+<div class="nav" on:click={startTyping}>
+  {#if typing}
+    <input
+      type="text"
+      class="nav"
+      id="input"
+      spellcheck="false"
+      bind:value={path}
+      use:init
+    />
+  {:else}
+    {#each path.split("/").filter((e) => e) as folder, i (i)}
+      <span
+        class="nav__item"
+        use:Ripple={{ ripple: true, color: "surface" }}
+        on:click={() => gotoPath(i)}>{folder}</span
+      >
+    {/each}
+  {/if}
+</div>
+
+<style lang="scss">
+  .nav {
+    font-family: "Roboto";
+    font-weight: 500;
+
+    background: #f6f6f6;
+
+    border-radius: 45px;
+    padding-right: 50px;
+
+    height: 40px;
+    width: 100%;
+
+    display: flex;
+    align-items: flex-start;
+
+    overflow: hidden;
+
+    cursor: text;
+    border: none;
+
+    box-sizing: border-box;
+    &#input {
+      padding-left: 20px;
+      padding-right: -50px;
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+  .nav__item {
+    display: flex;
+
+    height: 100%;
+    padding: 0 10px;
+
+    align-items: center;
+
+    user-select: none;
+    cursor: pointer;
+
+    border-right: 1px solid #bcbcbc;
+
+    &:nth-child(1) {
+      padding: 0 10px 0 20px;
+    }
+  }
+</style>
