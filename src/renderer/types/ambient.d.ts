d24ff3d235447fdf8173be63f48c327be7e2c4e4
Rafael <rafael.martinezcalvo@orange.fr>
2021-03-29 18:57:17 +0200

    added file type to renderer

--- src/renderer/types/ambient.d.ts
+++ src/renderer/types/ambient.d.ts
@@ -1,3 +1,15 @@
 declare interface MouseEvent {
   target: HTMLBaseElement;
 }
+
+declare interface IFile {
+  type: "directory" | "file" | "link" | "unknown";
+  hard_links: number;
+  owner: string;
+  group: string;
+  size: number;
+  last_modification_date: string;
+  last_modification_hour: string;
+  file_name: string;
+  link_to?: string;
+}
