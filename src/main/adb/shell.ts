497ef787ab4153ea652070564dd43a0bfbb03d7e
Rafael <rafael.martinezcalvo@orange.fr>
2021-04-03 11:12:47 +0200

    improved types of shell

--- src/main/adb/shell.ts
+++ src/main/adb/shell.ts
@@ -1,6 +1,9 @@
-import { exec } from "child_process";
+import { exec, ExecException } from "child_process";
 
-export async function shell(command: string, args?: string[]) {
+export async function shell(
+  command: string,
+  args?: string[]
+): Promise<{ err: ExecException | null; stdout: string; stderr: string }> {
   return new Promise((resolve, reject) => {
     exec(`adb shell "${command} ${args?.join(" ")}"`, (err, stdout, stderr) =>
       resolve({ err, stdout, stderr })
