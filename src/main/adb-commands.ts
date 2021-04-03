edb8bf5c4b269cd37efc64066dfa8d8c1d9ba768
Rafael <rafael.martinezcalvo@orange.fr>
2021-04-03 14:35:17 +0200

    done mime

--- src/main/adb-commands.ts
+++ src/main/adb-commands.ts
@@ -1,5 +1,4 @@
 import { ipcMain } from "electron";
-import { ExecException } from "child_process";
 import { ls, shell, getMime } from "./adb";
 import { basename } from "path";
 
@@ -8,23 +7,19 @@ ipcMain.handle("list-content", async (event, path: string) => {
 });
 
 ipcMain.handle("get-file-mime", async (event, path: string) => {
+  console.log("getting file mime");
+
   const fileMime = await getMime(path);
+  console.log("sending", fileMime);
+
   return fileMime;
 });
 
 ipcMain.handle("get-link-data", async (event, path: string) => {
-  let c = (await shell("readlink", [path])) as {
-    err: ExecException | null;
-    stdout: string;
-    stderr: string;
-  };
+  let c = await shell("readlink", [path]);
   let lastPath: string = "/";
   while (!c.err) {
-    c = (await shell("readlink", [c.stdout.trim()])) as {
-      err: ExecException | null;
-      stdout: string;
-      stderr: string;
-    };
+    c = await shell("readlink", [c.stdout.trim()]);
     if (c.err) {
       let found = (await ls(lastPath, true)).find(
         (e) => e.file_name == "."
--- src/main/adb/mime.ts
+++ src/main/adb/mime.ts
@@ -1,13 +1,15 @@
-import { Magic, MAGIC_MIME_TYPE } from "mmmagic";
+import fileType from "file-type";
 import { shell } from "./shell";
-const magic = new Magic(MAGIC_MIME_TYPE);
 
 export async function getMime(filePath: string) {
-  const { stdout } = await shell("xxd", ["-p", "-l4", filePath]);
+  const { stdout } = await shell("xxd", ["-p", "-l500", `'${filePath}'`]);
   const cleaned = stdout.replace(/[\s\n\r]+/g, "");
-  return new Promise((resolve) => {
-    magic.detect(Buffer.from(cleaned, "hex"), (error, result) => {
-      resolve(result);
-    });
-  });
+  const buffer = Buffer.from(cleaned, "hex");
+
+  const result = await fileType.fromBuffer(buffer);
+  console.log("found", result);
+
+  return result?.mime || "text/plain";
 }
+
+// getMime("/storage/C013-6544/&python/kivy.py");
